b6ca4e2d739648c416a9f1b16bdbec77994fac05
Vinitha Udaiyar <114304199+vinithaudaiyar@users.noreply.github.com>
2023-08-10 20:09:39 +0530

    Add files via upload

---
+++ number recognition.py
@@ -0,0 +1,36 @@
+import os #
+import cv2 #computer vision
+import numpy as np # array
+import matplotlib.pyplot as plt # visualization
+import tensorflow as tf # ml
+
+
+mnist = tf.keras.datasets.mnist
+(x_train ,y_train),(x_test,y_test) = mnist.load_data()
+
+#normalize
+#preprocessing
+x_train = tf.keras.utils.normalize(x_train,axis =1)
+x_test = tf.keras.utils.normalize(x_test,axis =1)
+
+
+# model=tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+# model.add(tf.keras.layers.Dense(128,activation='relu'))
+# model.add(tf.keras.layers.Dense(128,activation='relu'))
+# model.add(tf.keras.layers.Dense(10,activation='softmax'))
+
+# model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+# model.fit(x_train,y_train,epochs=3)
+# model.save('handwritten.model')
+
+model=tf.keras.models.load_model('handwritten.model')
+loss,accuracy=model.evaluate(x_test,y_test)
+
+print(loss)
+print(accuracy)
+
+
+
+
